0148f6f233fc341864e5fbf537f885633944154b
J-Pozenato <jppozenato@gmail.com>
2022-05-16 14:37:08 -0300

    pessoa

---
+++ dotNetGFT/Demos/Program.cs
@@ -0,0 +1,23 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Pedora";
+        p1.Idade = 20;
+        p1.Documento = "asiuhiu223";
+
+        Console.WriteLine(p1.Nome);
+
+        TrocarNome(p1, "Pedro");
+
+        Console.WriteLine(p1.Nome);
+
+    }
+
+    static void TrocarNome(Pessoa pessoa, string nome)
+    {
+        pessoa.Nome = nome;
+    }
+}
