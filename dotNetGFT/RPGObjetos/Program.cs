43d373886be035c57c8dd3400436438403a82001
J-Pozenato <jppozenato@gmail.com>
2022-05-25 13:39:06 -0300

    NonCombatant class

--- dotNetGFT/RPGObjetos/Program.cs
+++ dotNetGFT/RPGObjetos/Program.cs
@@ -1,3 +1,5 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RPGObjetos
+{
+    // See https://aka.ms/new-console-template for more information
+    Console.WriteLine("Hello, World!");
+}
