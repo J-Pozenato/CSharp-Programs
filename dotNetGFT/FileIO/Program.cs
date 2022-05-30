5eb78fd6f22859e27d0f0f6f3cdcc3e778abe255
J-Pozenato <jppozenato@gmail.com>
2022-05-30 13:58:41 -0300

    Directory file input and output

--- dotNetGFT/FileIO/Program.cs
+++ dotNetGFT/FileIO/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FileIO
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            Console.WriteLine("Enter the File name: ");
+            var name = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(name))
+            {
+                name = "default";
+            }
+            
+            name = RemoveInvalidChars(name);
+
+            var path = Path.Combine(Environment.CurrentDirectory, $"{name}.txt");
+            
+            CreateFile(path);
+
+            Console.WriteLine("Press enter to close");
+            Console.ReadLine();
+
+
+            static string RemoveInvalidChars(string name)
+            {
+                foreach (var character in Path.GetInvalidFileNameChars())
+                {
+                    name = name.Replace(character, '-');
+                }
+                
+                return name;
+            }
+            static void CreateFile(string path)
+            {
+                try
+                {
+                    using var sw = File.CreateText(path);
+
+                    sw.WriteLine("line1");
+                    sw.WriteLine("line2");
+                    sw.WriteLine("line3");
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid file name");
+                }
+
+            }
+
+
+        }
+
+    } 
+}
