d302db7a11b19db9342180204751f504b0aed902
J-Pozenato <jppozenato@gmail.com>
2022-05-31 20:10:47 -0300

    file system watcher

--- dotNetGFT/FileSystemWatcher/Program.cs
+++ dotNetGFT/FileSystemWatcher/Program.cs
@@ -1,4 +1,33 @@
 ﻿var path = @"F:\Csharp\dotNetGFT\DirectoryIO\globe";
 using var fsw = new FileSystemWatcher(path);
 
+fsw.Created += OnCreated;
+fsw.Created += OnDeleted;
+fsw.Created += OnRenamed;
 
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+Console.WriteLine(@"Watching the folder  F:\Csharp\dotNetGFT\DirectoryIO\globe ");
+Console.WriteLine("Press Enter to stop and close");
+
+Console.ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"A file was created : {e.Name}");
+}
+
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"A file was Deleted : {e.Name}");
+
+}
+
+
+void OnRenamed(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"A file was Renamed : {e.Name}");
+}
