4f6913c1ec5c70ceb80a5dff1cd8cf837ce5163d
J-Pozenato <jppozenato@gmail.com>
2022-06-01 13:32:15 -0300

    csvClass Reader

--- dotNetGFT/ProjCsvHelper/Program.cs
+++ dotNetGFT/ProjCsvHelper/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+// ReadCSVDynamic();
+ReadCSVClass();
+
+static void ReadCSVClass()
+{
+    var path = Path.Combine(Environment.CurrentDirectory,"users.csv");
+
+    var fi = new FileInfo(path);
+
+    if (!fi.Exists)
+    {
+        throw new FileNotFoundException($"File {path} not found");
+    }
+
+    var sr = new StreamReader(fi.FullName);
+    var csvConfig = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture);
+    using var csvReader = new CsvReader(sr, csvConfig);
+
+    var regs = csvReader.GetRecords<User>();
+    foreach (var reg in regs)
+    {
+        Console.WriteLine($"name: {reg.name}");
+        Console.WriteLine($"Telefone number: {reg.number}");
+        Console.WriteLine($"Email: {reg.email}");
+        Console.WriteLine("------------------");
+
+    }
+}
+
+
+static void ReadCSVDynamic()
+{
+    var path = Path.Combine(Environment.CurrentDirectory,"products.csv");
+
+    var fi = new FileInfo(path);
+
+    if (!fi.Exists)
+    {
+        throw new FileNotFoundException($"File {path} not found");
+    }
+
+    var sr =new StreamReader(fi.FullName);
+    var csvConfig = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture);
+    using var csvReader = new CsvReader(sr, csvConfig);
+
+    var regs = csvReader.GetRecords<dynamic>();
+    foreach (var reg in regs)
+    {
+        Console.WriteLine($"name: {reg.name}");
+        Console.WriteLine($"brand: {reg.brand}");
+        Console.WriteLine($"price: {reg.price}");
+        Console.WriteLine("------------------");
+
+    }
+}
---
+++ dotNetGFT/ProjCsvHelper/models/User.cs
@@ -0,0 +1,6 @@
+public class User
+{
+    public string name {get;set;}
+    public string email {get;set;}
+    public int number {get;set;}
+}
